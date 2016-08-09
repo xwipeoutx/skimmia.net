8df43bacc8abfb4f2a8ce92365290bbb8e975a43
XwipeoutX <xwipeoutx@gmail.com>
2016-08-09 23:12:30 +1000

    Trying new dsl

--- Skimmia.NUnit3.Tests/SampleTests.cs
+++ Skimmia.NUnit3.Tests/SampleTests.cs
@@ -7,39 +7,6 @@ using static Skimmia.SkimmiaBase;
 
 namespace Skimmia.NUnit3.Tests
 {
-    public class SkimmiaException : AggregateException
-    {
-        private readonly Test _root;
-        public IEnumerable<Test> FailingTests { get; }
-
-        public SkimmiaException(Test root)
-            : this(root.Leaves.Where(t => !t.HasPassed).ToList())
-        {
-            _root = root;
-        }
-
-        private SkimmiaException(IReadOnlyCollection<Test> failingTests) 
-            : base(failingTests.Select(e => e.Error))
-        {
-            FailingTests = failingTests;
-        }
-
-        public override string Message
-        {
-            get
-            {
-                return $"\n{_root.Name}\n" 
-                    + FailingTests.Aggregate(string.Empty, (previous, test) => $"{previous}  {FullName(test)}\n{test.Error.Message}\n\n");
-            }
-        }
-
-        public string FullName(Test test)
-        {
-            return test.Parent == _root 
-                ? test.Name 
-                : FullName(test.Parent) + "; " + test.Name;
-        }
-    }
 
     public class SampleTests
     {
