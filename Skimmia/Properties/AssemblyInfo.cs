a1e5102bc187859749fba2aded7d2930373bb18f
XwipeoutX <xwipeoutx@gmail.com>
2016-08-31 23:23:50 +1000

    Adds terrible nuspec files

--- Skimmia/Properties/AssemblyInfo.cs
+++ Skimmia/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Skimmia")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Hierarchical test runner")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Skimmia")]
